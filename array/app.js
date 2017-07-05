f6a1bb6a0bc3324721254b9ada8760d971f240ea
isam14 <imuck14@gmail.com>
2017-07-05 14:05:03 +0200

    exercice 1 à 6

--- array/app.js
+++ array/app.js
@@ -1,1 +1,29 @@
+
 var array = ['Pomme','Cerise','Tomate','Citron', ['Orange','Poivron']];
+
+//##Exercice 1
+var mois = ["janvier","fevrier","mars",
+"avril","mai","juin","juillet","aout","septembre","octobre","novembre","decembre"];
+
+//##Exercice 2
+	console.log(mois[2]);
+//##Exercice 3
+	console.log(mois[5]);
+//##Exercice 4
+	console.log(mois[10]);
+//##Exercice 5
+	mois[7] = "août";
+	console.log(mois[7]);
+//##Exercice 6
+//for (var i = 0; i <= 11; i++) {
+//	console.log(mois[0,11]);};
+var annee = mois.join();
+	console.log(annee);
+//##Exercice 7
+	
+
+
+
+
+
+
